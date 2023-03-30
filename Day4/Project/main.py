b01359bcf2b8037c97c242c2cbb41ee071dc6d9e
Jose Almir <joseamfj@outlook.com>
2023-03-30 14:22:44 -0300

    Finalize project day 4

--- Day4/Project/main.py
+++ Day4/Project/main.py
@@ -27,16 +27,20 @@ scissors = '''
 ---.__(___)
 '''
 game_images = [paper, scissors, rock]
-choice_option = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
-if  choice_option < 0 or choice_option > 2:
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
+if  user_choice < 0 or user_choice > 2:
     print("You entered an incorrect option, you lose!")
     quit()
 
-print(game_images[choice_option])
+print(game_images[user_choice])
 
 computer_choice = randint(0,2)
 print(f"Computer chose:\n{game_images[computer_choice]}")
-
-
+if (computer_choice == 0 and user_choice == 2) or (computer_choice == 1 and user_choice == 0) or (computer_choice == 2 and user_choice == 1):
+    print("You lose!!")
+elif computer_choice == user_choice:
+    print("Draw!!")
+else:
+    print("You win!!")
 
 
