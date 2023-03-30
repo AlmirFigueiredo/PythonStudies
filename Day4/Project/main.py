a8cc4b632bd29abaac25198184e60e036c4b6ca0
Jose Almir <joseamfj@outlook.com>
2023-03-30 14:17:48 -0300

    Create option to quit the game, if the users input is wrong

--- Day4/Project/main.py
+++ Day4/Project/main.py
@@ -1,4 +1,4 @@
-from random import randit
+from random import randint
 
 rock = '''
     _______
@@ -27,10 +27,16 @@ scissors = '''
 ---.__(___)
 '''
 game_images = [paper, scissors, rock]
-choice_option = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors"))
+choice_option = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
+if  choice_option < 0 or choice_option > 2:
+    print("You entered an incorrect option, you lose!")
+    quit()
+
 print(game_images[choice_option])
 
-computer_choice = randit(0,2)
+computer_choice = randint(0,2)
+print(f"Computer chose:\n{game_images[computer_choice]}")
+
 
 
 
