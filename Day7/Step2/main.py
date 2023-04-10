cfc5c6ad53075f9d5abb71c3df5180a980938520
Jose Almir <joseamfj@outlook.com>
2023-04-10 09:32:04 -0300

    Complete step 2 of the day 7 project

--- Day7/Step2/main.py
+++ Day7/Step2/main.py
@@ -5,11 +5,24 @@ chosen_word = choice(word_list)
 display = []
 for i in range(len(chosen_word)):
     display.append("_")
-    
-guess = input("Guess the letter: ").lower()
-for pos in range(len(display)):
-    if chosen_word[pos] == guess:
-        print("Right")
-        display[pos] = guess
-    
-print(display)
+corrects = 0
+wrongs = 0
+while corrects < len(chosen_word) and wrongs < 4:
+    guess = input("Guess the letter: ").lower()
+    k = 0
+    for i in range(len(chosen_word)):
+        if guess == chosen_word[i]:
+            k += 1
+            display[i] = guess
+        
+    if k > 0:
+        print("Right!!")
+        corrects += 1
+    else:
+        print("Wrong!!")
+        wrongs += 1
+    print(display)
+if "_" not in display:
+    print("You Win!!")
+else:
+    print("You lose!")
